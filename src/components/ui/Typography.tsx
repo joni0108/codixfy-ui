b0e36f8af5b6063acb50d854a0a36fbb300122f3
joni0108 <trollpro0108@icloud.com>
2024-12-12 18:33:05 -0800

    Improved style of Typography: Now it underlines onHover event

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -27,19 +27,19 @@ function Typography({children, variant = "body1", className, url}: Props) {
     }
 
     const linkVariants = {
-        h1: <h1><Link href={url || ""} className={tw("text-4xl font-bold text-blue-500", className)}>{children}</Link></h1>,
-        h2: <h2><Link href={url || ""} className={tw("text-3xl font-bold text-blue-500", className)}>{children}</Link></h2>,
-        h3: <h3><Link href={url || ""} className={tw("text-2xl font-bold text-blue-500", className)}>{children}</Link></h3>,
-        h4: <h4><Link href={url || ""} className={tw("text-xl font-bold text-blue-500", className)}>{children}</Link></h4>,
-        h5: <h5><Link href={url || ""} className={tw("text-lg font-bold text-blue-500", className)}>{children}</Link></h5>,
-        h6: <h6><Link href={url || ""} className={tw("text-base font-bold text-blue-500", className)}>{children}</Link></h6>,
-        title: <h1><Link href={url || ""} className={tw("text-2xl font-bold text-blue-500", className)}>{children}</Link></h1>,
-        subtitle1: <h2><Link href={url || ""} className={tw("text-lg font-bold text-blue-500", className)}>{children}</Link></h2>,
-        subtitle2: <h3><Link href={url || ""} className={tw("text-base font-bold text-blue-500", className)}>{children}</Link></h3>,
-        body1: <p><Link href={url || ""} className={tw("text-base text-blue-500", className)}>{children}</Link></p>,
-        body2: <p><Link href={url || ""} className={tw("text-sm text-blue-500", className)}>{children}</Link></p>,
-        caption: <p><Link href={url || ""} className={tw("text-xs text-blue-500", className)}>{children}</Link></p>,
-        overline: <p><Link href={url || ""} className={tw("text-xs uppercase text-blue-500", className)}>{children}</Link></p>,
+        h1: <h1><Link href={url || ""} className={tw("text-4xl font-bold text-blue-500 hover:underline", className)}>{children}</Link></h1>,
+        h2: <h2><Link href={url || ""} className={tw("text-3xl font-bold text-blue-500 hover:underline", className)}>{children}</Link></h2>,
+        h3: <h3><Link href={url || ""} className={tw("text-2xl font-bold text-blue-500 hover:underline", className)}>{children}</Link></h3>,
+        h4: <h4><Link href={url || ""} className={tw("text-xl font-bold text-blue-500 hover:underline", className)}>{children}</Link></h4>,
+        h5: <h5><Link href={url || ""} className={tw("text-lg font-bold text-blue-500 hover:underline", className)}>{children}</Link></h5>,
+        h6: <h6><Link href={url || ""} className={tw("text-base font-bold text-blue-500 hover:underline", className)}>{children}</Link></h6>,
+        title: <h1><Link href={url || ""} className={tw("text-2xl font-bold text-blue-500 hover:underline", className)}>{children}</Link></h1>,
+        subtitle1: <h2><Link href={url || ""} className={tw("text-lg font-bold text-blue-500 hover:underline", className)}>{children}</Link></h2>,
+        subtitle2: <h3><Link href={url || ""} className={tw("text-base font-bold text-blue-500 hover:underline", className)}>{children}</Link></h3>,
+        body1: <p><Link href={url || ""} className={tw("text-base text-blue-500 hover:underline", className)}>{children}</Link></p>,
+        body2: <p><Link href={url || ""} className={tw("text-sm text-blue-500 hover:underline", className)}>{children}</Link></p>,
+        caption: <p><Link href={url || ""} className={tw("text-xs text-blue-500 hover:underline", className)}>{children}</Link></p>,
+        overline: <p><Link href={url || ""} className={tw("text-xs uppercase text-blue-500 hover:underline", className)}>{children}</Link></p>,
     }
 
     return (
